80621aefd67c924522f773a340d8c0fa89d87d4d
qz5zwangzihan1 <qz5zwangzihan1@outlook.com>
2026-05-18 13:40:40 +0800

    Add files via upload

---
+++ c++_template.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+#define fr1(i,a,b) for(int (i)=(a);(i)<=(b);(i)++)
+#define fr2(i,a,b) for(int (i)=(a);(i)>=(b);(i)--)
+#define fv(i,p) for(auto (i):(p))
+#define ll long long
+#define ull unsigned ll
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define st first
+#define nd second
+#define elif else if
+#define debug cout<<endl<<"-------------------------------------------------------------"<<endl
+using namespace std; 
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);cout.tie(NULL);
+	
+	return 0;
+}
